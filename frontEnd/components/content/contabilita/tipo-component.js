6ef1cda59e663db6ed9bc81c20da79efd9cb3df6
Zexal0807 <gallinar00@gmail.com>
2020-03-23 12:29:00 +0100

    Add tipoEntrate tipoUscite

--- frontEnd/components/content/contabilita/tipo-component.js
+++ frontEnd/components/content/contabilita/tipo-component.js
@@ -4,52 +4,26 @@ class TipoComponent extends ZexalComponent {
 
     _data = [];
 
-    _i = 0;
-
     addEvent() {
         const self = this;
         this.querySelector('.add-line').addEventListener("click", function() {
-            self._data[self._i] = { id: "", descrizione: "" };
-            self.querySelector('form > div').innerHTML += self._renderTipo(self._data[self._i], self._i);
-            self._i++;
-            self._addRemoveEvent();
+            self.querySelector('form > div').innerHTML += self._add();
         });
-        this._addRemoveEvent();
         this.querySelector('form').addEventListener('submit', function(e) {
             self.sendData(e);
         });
     }
-
-    _addRemoveEvent() {
-        var self = this;
-        $(this.querySelectorAll('i[data-row]')).unbind("click");
-        $(this.querySelectorAll('i[data-row]')).on("click", function() {
-            delete self._data[Number(this.getAttribute('data-row'))];
-            $(this.parentElement.parentElement).remove();
-        });
-        $(this.querySelectorAll('*[name]')).on("change keyup keydown", function() {
-            var k = this.name.split("][");
-            self._data[k[0].substr(1)]['descrizione'] = this.value;
-        });
-    }
-
 
     render() {
         super.render();
         this.addEvent();
     }
 
-    _renderTipo(el, i) {
+    _add() {
         return `
         <div class="form-group row">
             <label class="col-sm-3 col-form-label">Descizione tipo</label>
-            <input type="hidden" class="form-control" name="[` + i + `][id]" required value="` + el.id + `">
-            <div class="col-sm-8">
-                <input type="text" class="form-control" name="[` + i + `][descrizione]" placeholder="Tipo entrata" required value="` + el.descrizione + `">
-            </div>
-            <div class="col-sm-1">
-                <i data-row="` + i + `" class="fa fa-times"></i>
-            </div>
+            <input class="col-sm-9 form-control" type="text" name="descrizione[]" required>
         </div>
     `;
     }
@@ -59,8 +33,13 @@ class TipoComponent extends ZexalComponent {
             <form><div>`;
         var self = this;
         this._data.forEach(el => {
-            r += self._renderTipo(el, self._i);
-            self._i++;
+            r += `
+            <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Descizione tipo</label>
+                <div class="col-sm-9">
+                    <div class="form-control">` + el.descrizione + `</div>
+                </div>
+            </div>`
         });
         r += `</div>
             <i class="add-line fa fa-plus"></i>
@@ -96,11 +75,10 @@ export class TipoEntrateComponent extends TipoComponent {
 
     sendData(e) {
         e.preventDefault();
-
         $.ajax({
             type: "POST",
-            url: "/setTipoEntrate",
-            data: { data: this._data },
+            url: "/addTipoEntrate",
+            data: $(e.target).serializeArray(),
             dataType: "json",
             success: function(s) {
                 document.querySelector("app-content").connectedCallback();
@@ -137,11 +115,10 @@ export class TipoUsciteComponent extends TipoComponent {
 
     sendData(e) {
         e.preventDefault();
-
         $.ajax({
             type: "POST",
-            url: "/setTipoUscite",
-            data: { data: this._data },
+            url: "/addTipoUscite",
+            data: $(e.target).serializeArray(),
             dataType: "json",
             success: function(s) {
                 document.querySelector("app-content").connectedCallback();
