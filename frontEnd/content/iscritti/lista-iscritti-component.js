da7df0b251e725c75bb02e0ccdbd8db4be37e47e
Zexal0807 <gallinar00@gmail.com>
2020-05-17 10:38:01 +0200

    Implement liste

--- frontEnd/content/iscritti/lista-iscritti-component.js
+++ frontEnd/content/iscritti/lista-iscritti-component.js
@@ -24,6 +24,7 @@ class RigaIscritto extends ZexalComponent {
             <div class="col-md-3 d-none d-md-block">` + this._data.codice_fiscale + `</div>
         </div>`;
     }
+
     _addEvent() {
         var self = this;
         $(this).on("click", function() {
@@ -39,55 +40,24 @@ class ListaIscrittiComponent extends ZexalComponent {
 
     _data = [];
 
-    render() {
-        super.render();
-        var self = this;
-        $(this.querySelector('select')).on('change', function() {
-            self.setAnno($(this).val());
-        });
-        $('tr').on("click", function() {
-            $('app-content').html("");
-            $('app-content').append(new DatiIscrittoComponent(1));
-        });
-    }
+    _filterYear = true;
+    _year = (new Date()).getFullYear();
 
-    _render() {
-        var r = `
-        <select>
-            <option value="2020" selected>2020</option>
-        </select>
-
-        <table class="table table-striped">
-            <tr>
-                <th>Cognome</th>
-                <th>Nome</th>
-            </tr>`;
-
-        this._data.forEach(el => {
-            r += `<tr>
-                    <td>` + el.cognome + `</td>
-                    <td>` + el.nome + `</td>
-                </tr>`;
-        });
-        r += `
-        </table>`;
-
-        return r;
-    }
-}
-
-export class ListaSociComponent extends ListaIscrittiComponent {
+    _functionId = null;
+    _loadUrl = null;
 
     connectedCallback() {
-        const self = this;
+        var self = this;
         this.render();
         $.ajax({
             type: "POST",
             url: "/userFunction",
-            data: { id: 8 },
+            data: {
+                id: self._functionId
+            },
             dataType: "json",
             success: function() {
-                self.setAnno("2020");
+                self.loadData();
             },
             error: function(e) {
                 console.log(e);
@@ -96,14 +66,21 @@ export class ListaSociComponent extends ListaIscrittiComponent {
     }
 
     setAnno(a) {
+        this._year = a;
+        this.loadData();
+    }
+
+    loadData() {
         var self = this;
+        var d = {};
+        if (this._filterYear) {
+            d.anno = this._year;
+        }
         $.ajax({
             type: "POST",
-            url: "/getSoci",
-            data: {
-                anno: a
-            },
+            url: self._loadUrl,
             dataType: "json",
+            data: d,
             success: function(s) {
                 self._data = s;
                 self.render();
@@ -114,5 +91,71 @@ export class ListaSociComponent extends ListaIscrittiComponent {
         });
     }
 
+    render() {
+        super.render();
+        var self = this;
+        $(this._data).each(function(k, v) {
+            $(".data", self).append(new RigaIscritto(v));
+        });
+    }
+
+    _render() {
+        var r = ``;
+        if (this._filterYear) {
+            r += `<div class="col-12 filter">
+            <select>`;
+            var y = (new Date()).getFullYear();
+            for (let i = 2015; i <= y; i++) {
+                r += `<option value="` + i + `"`;
+                if (i == this._year) {
+                    r += ` selected`;
+                }
+                r += `>` + i + `</option>`
+            }
+            r += `</select>
+        </div>`;
+        }
+        r += `<div class="data"></div>`;
+
+        return r;
+    }
+
+    _addEvent() {
+        var self = this;
+        if (this._filterYear) {
+            $(this.querySelector('select')).on('change', function() {
+                self._year = $(this).val();
+                self.setAnno($(this).val());
+            });
+        }
+    }
+
+}
+
+export class ListaSociComponent extends ListaIscrittiComponent {
+
+    _functionId = 8;
+    _loadUrl = "/getSoci";
+
 }
 customElements.define("lista-soci", ListaSociComponent);
+
+
+export class ListaTesseratiComponent extends ListaIscrittiComponent {
+
+    _functionId = 8;
+    _loadUrl = "/getTesserati";
+
+}
+customElements.define("lista-tesserati", ListaTesseratiComponent);
+
+
+export class ListaAssociatiComponent extends ListaIscrittiComponent {
+
+    _filterYear = false;
+
+    _functionId = 8;
+    _loadUrl = "/getAssociati";
+
+}
+customElements.define("lista-associati", ListaAssociatiComponent);
