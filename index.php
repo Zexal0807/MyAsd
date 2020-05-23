58a3deb49c90a8e0682c7d24e5d73ef4b749e9a2
Zexal0807 <gallinar00@gmail.com>
2020-05-23 09:57:22 +0200

    Add loadImage route

--- index.php
+++ index.php
@@ -16,6 +16,22 @@ ZRoute::get("/logout", function ($data){
 	redirect("/");
 });
 
+ZRoute::get("/load-image/<id>", function ($data){
+	require_once 'backEnd/classes/Database.php';
+	$DB = new Database($_SESSION['db_host'], $_SESSION['db_user'], $_SESSION['db_pasw'], $_SESSION['db_db']);
+
+	$ret = $DB->select("*")
+		->from("file")
+		->where("id", "=", $data['id'])
+		->execute();
+		
+	if(sizeof($ret) == 1){
+		$ext = substr($ret[0]['nome'], find(".", $ret[0]['nome']) + 1);
+		header('Content-type: image/'.$ext);
+		echo $ret[0]['data']; 
+	}
+});
+
 require 'backEnd/api.php';
 
 ZRoute::listen();
