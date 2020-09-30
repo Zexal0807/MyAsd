16ffa90cb6b17bbfb95ef9b25384374f9dd6a101
Zexal0807 <gallinar00@gmail.com>
2020-09-30 19:47:49 +0200

    Fix - conferma tesseramento

--- backEnd/api/tesseramento/getTesseramentoComfermabile.php
+++ backEnd/api/tesseramento/getTesseramentoComfermabile.php
@@ -11,13 +11,10 @@ class getTesseramentoComfermabile{
 
         $DB = new Database($_SESSION['db_host'], $_SESSION['db_user'], $_SESSION['db_pasw'], $_SESSION['db_db']);
         $a = intval(date("m")) > 7 ? intval(date("Y"))+1:intval(date("Y"));
-        
         $ret = $DB->executeSql('SELECT tesseramenti.idIscritto AS id, tesseramenti.codice AS code, anagrafiche.cognome, anagrafiche.nome
         FROM tesseramenti 
         INNER JOIN anagrafiche ON tesseramenti.idIscritto = anagrafiche.idIscritto
-        WHERE (tesseramenti.idTipoTesseramento = "2'.$a.'" OR tesseramenti.idTipoTesseramento = "4'.$a.'") AND tesseramenti.idFile IS NOT NULL AND tesseramenti.idIscritto NOT IN (
-            SELECT id FROM gettesserati WHERE anno = YEAR(NOW()) 
-        )');
+        WHERE SUBSTR(tesseramenti.idTipoTesseramento,1,1) IN ("2","4") AND SUBSTR(tesseramenti.idTipoTesseramento,2,4) = "'.$a.'" AND tesseramenti.codice IS NOT NULL');
         
         return $ret;
     }
